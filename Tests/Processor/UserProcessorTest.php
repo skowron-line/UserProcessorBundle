e79f61db83b3955831f83a51fbeb30a8e863d5ed
krzysztof skaradzinski <wygladaszjak@gmail.com>
2016-05-17 09:44:33 +0200

    improved tests

--- Tests/Processor/UserProcessorTest.php
+++ Tests/Processor/UserProcessorTest.php
@@ -4,6 +4,8 @@ namespace Skowronline\UserProcessorBundle\Tests\Processor;
 
 use Skowronline\UserProcessorBundle\Processor\UserProcessor;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @author Krzysztof Skaradziński <skaradzinski.krzysztof@gmail.com>
@@ -12,6 +14,106 @@ class UserProcessorTest extends \PHPUnit_Framework_TestCase
 {
     public function testProcessorWithoutToken()
     {
+        $processor = new UserProcessor($this->mockTokenStorage());
+        $this->assertSame([], $processor([]));
+
+        $record = ['data' => '2015-06-01 12:00:00'];
+        $this->assertSame($record, $processor($record));
+    }
+
+    public function testProcessorWithoutUser()
+    {
+        $token = $this->mockTokenInterface();
+        $storage = $this->mockTokenStorage($token);
+
+        $processor = new UserProcessor($storage);
+        $this->assertSame([], $processor([]));
+
+        $record = ['date' => '2015-06-01 12:00:00'];
+        $this->assertSame($record, $processor($record));
+    }
+
+    public function testUserAsString()
+    {
+        $token = $this->mockTokenInterface('skowron-line');
+        $storage = $this->mockTokenStorage($token);
+
+        $processor = new UserProcessor($storage);
+        $this->assertSame(
+            [
+                'extra' => ['user' => 'skowron-line']
+            ],
+            $processor([])
+        );
+
+        $record = [
+            'date' => '2015-06-01 12:00:00',
+            'extra' => [
+                'branch' => 'master'
+            ]
+        ];
+
+        $this->assertSame(
+            [
+                'date' => '2015-06-01 12:00:00',
+                'extra' => [
+                    'branch' => 'master',
+                    'user'   => 'skowron-line'
+                ]
+            ],
+            $processor($record)
+        );
+    }
+
+    public function testUserAsObject()
+    {
+        /** @var UserInterface||PHPUnit_Framework_MockObject_MockObject $user */
+        $user = $this
+            ->getMockBuilder(UserInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $user
+            ->method('getUserName')
+            ->willReturn('skowron-line');
+
+        $token = $this->mockTokenInterface($user);
+        $storage = $this->mockTokenStorage($token);
+
+        $processor = new UserProcessor($storage);
+        $this->assertSame(
+            [
+                'extra' => ['user' => 'skowron-line']
+            ],
+            $processor([])
+        );
+
+        $record = [
+            'date' => '2015-06-01 12:00:00',
+            'extra' => [
+                'branch' => 'master'
+            ]
+        ];
+
+        $this->assertSame(
+            [
+                'date' => '2015-06-01 12:00:00',
+                'extra' => [
+                    'branch' => 'master',
+                    'user'   => 'skowron-line'
+                ]
+            ],
+            $processor($record)
+        );
+    }
+
+    /***
+     * @param null $returnValue
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|TokenStorageInterface
+     */
+    private function mockTokenStorage($returnValue = null)
+    {
         /** @var TokenStorageInterface|\PHPUnit_Framework_MockObject_MockObject $tokenStorage */
         $tokenStorage = $this
             ->getMockBuilder(TokenStorageInterface::class)
@@ -19,14 +121,30 @@ class UserProcessorTest extends \PHPUnit_Framework_TestCase
             ->getMock();
 
         $tokenStorage
-            ->expects($this->once())
             ->method('getToken')
-            ->willReturn(null);
+            ->willReturn($returnValue);
 
-        $record = [];
+        return $tokenStorage;
+    }
 
-        $processor = new UserProcessor($tokenStorage);
-        $this->assertSame([], $processor($record));
+    /**
+     * @param null $returnValue
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|TokenInterface
+     */
+    private function mockTokenInterface($returnValue = null)
+    {
+        /** @var TokenInterface|\PHPUnit_Framework_MockObject_MockObject $token */
+        $token = $this
+            ->getMockBuilder(TokenInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $token
+            ->method('getUser')
+            ->willReturn($returnValue);
+
+        return $token;
     }
 }
 
